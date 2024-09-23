6832ed54a9d89758cdfd560d08de01737761ce7a
Adityaa Mehra <adirakash@gmail.com>
2024-09-24 00:32:56 +0530

    final

--- BO-Motor/BO-Motor.ino
+++ BO-Motor/BO-Motor.ino
@@ -1,4 +1,18 @@
-//MOTOR1 PINS
+/*
+* in3 (HIGH) := Is going backwards of right when looking from the perspective of ultrasonic
+* in1 (HIGH) := Is going backwards of left when looking from the perspective of ultrasonic
+*/
+
+const int ispr = 11;
+const int ispm = 12;
+const int ispl = 13; // Pin where the IR sensor is connected
+int ivm = 0;
+int ivl = 0;
+int ivr = 0;   // Variable to store the value from the sensor
+
+long dur;
+int dis;
+
 const int ena = 5;
 const int in1 = 6;
 const int in2 = 7;
@@ -7,88 +21,110 @@ const int in3 = 8;
 const int in4 = 9;
 const int enb = 10;
 
+const int tp = 3;
+const int ep = 4;
+
+void left()
+{
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,LOW);
+    analogWrite(enb,150);
+    digitalWrite(in3,LOW);
+    digitalWrite(in4,HIGH);
+}
+
+void right()
+{
+    analogWrite(ena,150);
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,HIGH);
+    digitalWrite(in3,LOW);
+    digitalWrite(in4,LOW);
+}
+
+void stop()
+{
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,LOW);
+    digitalWrite(in3,LOW);
+    digitalWrite(in4,LOW);
+}
+
+void backward()
+{
+    analogWrite(ena,150);
+    analogWrite(enb,150);
+    digitalWrite(in1,HIGH);
+    digitalWrite(in2,LOW);
+    digitalWrite(in3,HIGH);
+    digitalWrite(in4,LOW);
+}
+
+void forward()
+{
+    analogWrite(ena,150);
+    analogWrite(enb,150);
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,HIGH);
+    digitalWrite(in3,LOW);
+    digitalWrite(in4,HIGH);
+}
+
 void setup() {
   // Define pin modes
+  pinMode(ispm, INPUT);
+  pinMode(ispl, INPUT);
+  pinMode(ispr, INPUT);
+  pinMode(tp,OUTPUT);
+  pinMode(ep,INPUT);
   pinMode(ena, OUTPUT);
   pinMode(enb, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  //MOTOR_A CLOCKWISE MAX SPEED
-  digitalWrite(in1,HIGH);
-  digitalWrite(in2,LOW);
-  analogWrite(ena, 255);
+  ivm = digitalRead(ispm); // Read the value from the sensor
+  ivl = digitalRead(ispl);
+  ivr = digitalRead(ispr);
 
-  //MOTOR_B CLOCKWISE MAX SPEED
-  digitalWrite(in3,HIGH);
-  digitalWrite(in4,LOW);
-  analogWrite(enb, 255);
-  delay(1000);
+  digitalWrite(tp,LOW);
+  digitalWrite(tp,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(tp,LOW);
+  delayMicroseconds(2);
+  
+  dur = pulseIn(ep,HIGH);
 
-  //STOP
-  digitalWrite(in1,LOW);
-  digitalWrite(in2,LOW);
-  digitalWrite(in3,LOW);
-  digitalWrite(in4,LOW);
-  delay(500);
-
-  //MOTOR_A COUNTERCLOCKWISE MAX SPEED
-  digitalWrite(in1,LOW);
-  digitalWrite(in2,HIGH);
-  analogWrite(ena, 255);
-
-  //MOTOR_B COUNTERCLOCKWISE MAX SPEED
-  digitalWrite(in3,LOW);
-  digitalWrite(in4,HIGH);
-  analogWrite(enb, 255);
-
-  delay(1000);
-
-  //STOP
-  digitalWrite(in1,LOW);
-  digitalWrite(in2,LOW);
-  digitalWrite(in3,LOW);
-  digitalWrite(in4,LOW);
-  delay(500);
-
-
-  //MOTOR_A TURNS RIGHT MAX SPEED
-  digitalWrite(in1,HIGH);
-  digitalWrite(in2,LOW);
-  analogWrite(ena, 255);
-
-  //MOTOR_B CLOCKWISE HALF SPEED
-  // digitalWrite(in3,HIGH);
-  // digitalWrite(in4,LOW);
-  // analogWrite(enb, 127);
-  delay(1000);
-
-  //STOP
-  digitalWrite(in1,LOW);
-  digitalWrite(in2,LOW);
-  digitalWrite(in3,LOW);
-  digitalWrite(in4,LOW);
-  delay(500);
-
-  //MOTOR_A COUNTERCLOCKWISE MAX SPEED
-  // digitalWrite(in1,LOW);
-  // digitalWrite(in2,HIGH);
-  // analogWrite(ena, 127);
-
-  //MOTOR_B COUNTERCLOCKWISE MAX SPEED
-  digitalWrite(in3,HIGH);
-  digitalWrite(in4,LOW);
-  analogWrite(enb, 255);
-  delay(1000);
-
-  //STOP
-  digitalWrite(in1,LOW);
-  digitalWrite(in2,LOW);
-  digitalWrite(in3,LOW);
-  digitalWrite(in4,LOW);
-  delay(8000); 
+  if(dur !=0)
+  {
+    dis=dur*0.034/2;
+    if(dis < 10)
+    {
+      stop();
+    }
+    else
+    {
+      if(ivm == LOW && ivr == HIGH && ivl == HIGH)
+      {
+        forward();
+      }
+      else if (ivm == LOW && ivr == HIGH && ivl == LOW)
+      {
+        left();
+      }
+      else if (ivm == LOW && ivr == LOW && ivl == HIGH)
+      {
+        right();
+      }
+      else
+      {
+        stop();
+      }
+    }
+  }
 }
